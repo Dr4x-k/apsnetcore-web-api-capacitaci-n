b48fa3ac6d0b6e1e7c613f8dafbd6b781c814e33
Ju4nM <jenriquemflores@gmail.com>
2024-10-07 11:57:09 -0700

    Referencias de proyectos agregadas

---
+++ capacitacion/Controllers/TareaController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace capacitacion.Controllers {
+  [Route ("api/[controller]")]
+  [ApiController]
+  public class TareaController : ControllerBase {
+
+    [HttpGet]
+    public IEnumerable<string> FindAll () {
+      return new string[] { "value1", "value2" };
+    }
+
+    [HttpGet ("{id}")]
+    public string FindOne (int id) {
+      return "value";
+    }
+
+    [HttpPost]
+    public void Create ([FromBody] string value) {
+    }
+
+    [HttpPut ("{id}")]
+    public void Update (int id, [FromBody] string value) {
+    }
+
+    [HttpDelete ("{id}")]
+    public void Remove (int id) {
+    }
+  }
+}
---
+++ capacitacion/Controllers/UsuarioController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace capacitacion.Controllers {
+  [Route ("api/[controller]")]
+  [ApiController]
+  public class UsuarioController : ControllerBase {
+
+    [HttpGet]
+    public IEnumerable<string> FindAll () {
+      return new string[] { "value1", "value2" };
+    }
+
+    [HttpGet ("{id}")]
+    public string FindOne (int id) {
+      return "value";
+    }
+
+    [HttpPost]
+    public void Create ([FromBody] string value) {
+    }
+
+    // PUT api/<UsuarioController>/5
+    [HttpPut ("{id}")]
+    public void Update (int id, [FromBody] string value) {
+    }
+
+    // DELETE api/<UsuarioController>/5
+    [HttpDelete ("{id}")]
+    public void Remove (int id) {
+    }
+  }
+}
